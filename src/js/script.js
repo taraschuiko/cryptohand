daceedcf81885c6601204a0da5f1ad3fa18a2e2c
Taras Chuiko <taraschuiko@gmail.com>
2018-05-01 18:19:49 +0300

    Created challenge and live play

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function($) {
+	var name = "";
+	var matchLength = 0;
+	var etherumAmount = 0;
+	var height = 0;
+
 	// Slider
 	$(".main-slider").slick({
 		dots: false,
@@ -53,12 +58,12 @@ jQuery(document).ready(function($) {
 		var amount = $(amountSelector).text();
 
 		var hideSelectors = nameSelector + ', ' + amountSelector + ', ' +
-			'#' + $(this).closest('.main-slider-item').attr('id') + ' .main-slider-risk' + ', ' +
-			'#' + $(this).closest('.main-slider-item').attr('id') + ' .slider-item-middle' + ', ' +
-			'#' + $(this).closest('.main-slider-item').attr('id') + ' .divider' + ', ' +
-			'#' + $(this).closest('.main-slider-item').attr('id') + ' .pick-hand' + ', ' +
-			'#' + $(this).closest('.main-slider-item').attr('id') + ' .tiebreaker' + ', ' +
-			'#' + $(this).closest('.main-slider-item').attr('id') + ' .btn-challenge';
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .main-slider-risk' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .slider-item-middle' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .divider' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .pick-hand' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .tiebreaker' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .btn-challenge';
 		$(this).closest('.main-slider-item').css('height', height);
 		$(hideSelectors).css('display', 'none');
 
@@ -75,10 +80,77 @@ jQuery(document).ready(function($) {
 		playLiveButton = '#' + $(this).closest('.main-slider-item').attr('id') + ' .btn-live';
 		$(playLiveButton).html('Play Live');
 		$(playLiveButton).css({'background': 'rgba(34, 139, 34, 0.6)',
-							'color': '#fff',
-							'padding': '8px',
-							'cursor': 'pointer',
-							'width': '20%',
-							'margin': '20px 40%'});
+			'color': '#fff',
+			'padding': '8px',
+			'cursor': 'pointer',
+			'width': '20%',
+			'margin': '20px 40%'});
+	});
+
+	$(document).on('click', '.btn-live', '.btn-live', function(event) {
+		$(this).closest('.main-slider-item').addClass('main-slider-live');
+
+		height = $(this).closest('.main-slider-item').outerHeight() - 30;
+
+		var nameSelector = '#' + $(this).closest('.main-slider-item').attr('id') + ' .main-slider-name';
+		name = $(nameSelector).text();
+
+		var amountSelector = '#' + $(this).closest('.main-slider-item').attr('id') + ' .main-slider-amount';
+		var amount = $(amountSelector).text();
+
+		var hideSelectors = nameSelector + ', ' + amountSelector + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .main-slider-risk' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .slider-item-middle' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .divider' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .pick-hand' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .tiebreaker' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .slider-item-buttons' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .challenge-message' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .btn-live';
+		$(this).closest('.main-slider-item').css('height', height);
+		$(hideSelectors).css('display', 'none');
+
+		$(this).closest('.main-slider-item').find('.name-amount').append($.parseHTML(
+			'<b><p style="text-decoration: underline">' + name + ' Requirements:</p><p>Match Length: <span class="match-length"></span></p><p>Etherum: <span class="match-etherum"></span></p></b>'
+			));
+
+		$(this).closest('.main-slider-item').append($.parseHTML(
+			'<p>Level: </p><div class="match-length-choose"><p>Match length:</p><button class="match-length-btn">3</button><button class="match-length-btn">7</button><button class="match-length-btn">11</button></div>'
+			));
+
+		$(this).closest('.main-slider-item').append($.parseHTML(
+			'<div class="etherum-amount"><p>Etherum: </p><input type="number" placeholder="Type Amount" class="etherum-amount-input"></div>'
+			));
+
+		$(this).closest('.main-slider-item').append($.parseHTML(
+			'<button class="send-request-btn">Send Request</button>'
+			));
+	});
+
+	$(document).on('click', '.match-length-btn', function(event) {
+		$(this).css('background', 'rgba(256, 256, 256, 0.2)');
+		$(this).siblings('.match-length-btn').each(function(index, el) {
+			$(this).css('background', 'rgba(256, 256, 256, 0.5)');
+		});
+	});
+
+	$(document).on('click', '.match-length-btn', function(event) {
+		matchLength = $(this).html();
+	});
+
+	$(document).on('click', '.send-request-btn', function(event) {
+		etherumAmount = $(this).closest('.main-slider-item').find('.etherum-amount-input').val();
+		var hideSelectors = '#' + $(this).closest('.main-slider-item').attr('id') + ' p' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .match-length-choose' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .etherum-amount' + ', ' +
+		'#' + $(this).closest('.main-slider-item').attr('id') + ' .send-request-btn';
+		$(hideSelectors).css('display', 'none');
+		$(this).closest('.main-slider-item').find('.name-amount').append($.parseHTML(
+			'<p>Waiting for ' + name + ' Confirmation</p>' +
+			'<p class="waiting-stats">'+matchLength+'</p><p class="waiting-stats">'+etherumAmount+'</p>'
+			));
+
+		$(this).closest('.main-slider-item').append($.parseHTML('<button class="waiting-cancel clearfix">Cancel</button>'));
+		
 	});
 });
